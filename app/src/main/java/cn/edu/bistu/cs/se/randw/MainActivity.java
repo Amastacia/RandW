9a96604618b7e4484107a2e91452e36fe1aa55fd
Amastacia <601027538@qq.com>
2017-09-12 22:25:18 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/bistu/cs/se/randw/MainActivity.java
@@ -0,0 +1,111 @@
+package cn.edu.bistu.cs.se.randw;
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
+import static android.R.attr.name;
+
+public class MainActivity extends AppCompatActivity {
+    private String FILE_NAME="MyFileName.txt";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Button buttonW=(Button)findViewById(R.id.buttonWrite);
+        buttonW.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override
+            public void onClick(View view) {
+                OutputStream out=null;
+                try {
+                    FileOutputStream fileOutputStream=openFileOutput(FILE_NAME,MODE_PRIVATE);
+                    out=new BufferedOutputStream(fileOutputStream);
+                    String content="WuYanMing 2015011226 1502";
+                    try {
+                        out.write(content.getBytes(StandardCharsets.UTF_8));
+                    }
+                    finally {
+                        if(out!=null)
+                            out.close();
+                    }
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+        Button buttonR=(Button)findViewById(R.id.buttonRead);
+        buttonR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                InputStream in=null;
+                try {
+                    FileInputStream fileInputStream = openFileInput(FILE_NAME);
+                    in=new BufferedInputStream(fileInputStream);
+                    int c;
+                    StringBuilder stringBuilder=new StringBuilder("");
+                    try{
+                        while ((c=in.read())!=-1) {
+                            stringBuilder.append((char)c);
+                        }
+                        Toast.makeText(MainActivity.this,stringBuilder.toString(),Toast.LENGTH_LONG).show();
+                    }
+                    finally {
+                        if(in!=null)
+                            in.close();
+                    }
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
